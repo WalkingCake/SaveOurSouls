476191127f38c5572faa17a8d4d59465f6318ac5
WalkingCake <mishayooo@mail.ru>
2020-12-01 23:56:21 +0500

    +Enemies fields of view (started)

---
+++ Assets/Scripts/Enemies/FieldOfView/FieldOfView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+namespace Assets.Scripts.Enemies.FieldOfView
+{
+    public class FieldOfView : MonoBehaviour
+    {
+        public event Action OnPlayerEntered;
+        public event Action OnPLayerInside;
+        public event Action OnPlayerExited;
+       
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            Debug.Log($"{collision.transform.name} entered.");
+        }
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            Debug.Log($"{collision.transform.name} exited.");
+        }
+
+        [SerializeField] private Collider2D _collider;
+    }
+}
