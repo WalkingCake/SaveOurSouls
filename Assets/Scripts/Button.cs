50e7971a51f01b6398150cbdf7cba2b8222e8028
WalkingCake <mishayooo@mail.ru>
2020-12-10 23:10:44 +0500

    +Final

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SaveOurSouls
+{
+    public class Button : MonoBehaviour
+    {
+        private void OnMouseEnter()
+        {
+            this.transform.position += (Vector3)this._offset;
+        }
+        private void OnMouseExit()
+        {
+            this.transform.position -= (Vector3)this._offset;
+        }
+
+        private void OnMouseDown()
+        {
+            this._text.SetActive(!this._text.activeInHierarchy);
+        }
+
+
+
+        [SerializeField] private Vector2 _offset = new Vector2(0, 0.2f);
+        [SerializeField] private GameObject _text;
+    }
+}
